8bb54d95d076fb493e075c99697c434aa986c2a2
Mohit <mohitsaini3012003@gmail.com>
2023-02-04 15:28:39 +0530

    Team frontend

--- astrax23/views.py
+++ astrax23/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http import JsonResponse
-from .models import (Speaker,Sponsor)
+from .models import (Speaker,Sponsor,Team)
 
 def home(request):
     content ={
         'speakers_details':Speaker.objects.all(),
         'sponsors':Sponsor.objects.all(),
+        'team_members':Team.objects.all(),
     }
     return render(request,'base.html',content)
 
