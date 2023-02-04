cbb457aa087ba65c8cb6f4e97e69e685c6f5c8e8
Mohit <mohitsaini3012003@gmail.com>
2023-02-04 14:51:39 +0530

    Created the model for teams

---
+++ astrax23/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import (Speaker ,Sponsor,Team)
+admin.site.register(Speaker)
+admin.site.register(Sponsor)
+admin.site.register(Team)
+
---
+++ astrax23/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from django.http import JsonResponse
+from .models import (Speaker,Sponsor)
+
+def home(request):
+    content ={
+        'speakers_details':Speaker.objects.all(),
+        'sponsors':Sponsor.objects.all(),
+    }
+    return render(request,'base.html',content)
+
+
+
